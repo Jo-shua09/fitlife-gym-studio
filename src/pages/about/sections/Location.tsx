8c12edfb19ec35ff9d468d1004fe269de1816be8
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-05-03 21:04:32 +0100

    testing the embeded map

--- src/pages/about/sections/Location.tsx
+++ src/pages/about/sections/Location.tsx
@@ -14,12 +14,15 @@ const Location = () => {
             </address>
           </div>
 
-          <div className="w-full h-full overflow-x-hidden shadow-2xl">
+          <div className="w-full max-w-4xl mx-auto overflow-hidden rounded-xl shadow-2xl">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.05057513894!2d-74.3091634187714!3d40.69719335572737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sng!4v1746177119654!5m2!1sen!2sng"
-              width="600"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387193.05057513894!2d-74.3091634187714!3d40.69719335572737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sng!4v1746302535839!5m2!1sen!2sng"
+              width="100%"
               height="450"
+              style={{ border: 0 }}
+              allowFullScreen=""
               loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
         </div>
